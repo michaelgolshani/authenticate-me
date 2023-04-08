8211b19527b780769c204da1ed41caab59dbb2bf
Michael Golshani <michaelgolshani@gmail.com>
2023-04-07 22:49:54 -0700

    Authenticate-me Login Form, Phase 1

---
+++ frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import * as sessionActions from "../../store/session";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+import "./LoginForm.css";
+
+function LoginFormPage() {
+  const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state.session.user);
+  const [credential, setCredential] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({});
+
+  if (sessionUser) return <Redirect to="/" />;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors({});
+    return dispatch(sessionActions.login({ credential, password })).catch(
+      async (res) => {
+        const data = await res.json();
+        if (data && data.errors) setErrors(data.errors);
+      }
+    );
+  };
+
+  return (
+    <div className="login-form-container">
+      <h1 className="login-form-title">Log In</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username or Email
+          <input
+            type="text"
+            value={credential}
+            onChange={(e) => setCredential(e.target.value)}
+            required
+            className="login-form-input"
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            className="login-form-input"
+          />
+        </label>
+        {errors.credential && <p className ="errors">{errors.credential}</p>}
+        <button type="submit" className="login-form-button">
+          Log In
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginFormPage;
