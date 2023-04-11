98d10d41b9e1ec896856000cc912ffdd45d7f459
Michael Golshani <michaelgolshani@gmail.com>
2023-04-11 13:37:11 -0700

    close to completion on the getAllGroups page.

--- frontend/src/components/SeeAllGroupsPage/index.js
+++ frontend/src/components/SeeAllGroupsPage/index.js
@@ -1,5 +1,6 @@
+// SeeAllGroupsPage.js
 import React, { useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import './SeeAllGroupsPage.css'
 
@@ -7,45 +8,52 @@ import { getAllGroupsThunk } from '../../store/groups';
 
 export default function GetAllGroups() {
   const dispatch = useDispatch()
+  const history = useHistory()
+
 
   useEffect(() => {
     dispatch(getAllGroupsThunk())
   }, [dispatch])
 
-
   const groupSelector = useSelector((state) => state.group.allGroups)
-
-  console.log("GROUPS", groupSelector)
-  // if(!groups.allGroups) {
-  //   return <div>loading</div>
-  // }
 
   const groups = Object.values(groupSelector)
 
-  console.log("groups", groups)
-  return (
 
-    <>
-      <div>
-        <div>Events</div>
-        <div>Groups</div>
-        {
-          groups.map((group) => (
-            <>
-              <div className='group-elements'>
-                <div>{group.name}</div>
-                <div>{group.city}</div>
-                <div>{group.about}</div>
-              </div>
-            </>
-          ))
-        }
+
+
+  const handleGroupClick = (groupId) => {
+    history.push(`/groups/${groupId}`)
+  }
+
+
+
+  return (
+    <div className="group-list-container">
+
+      <div className="group-list-header">
+          <NavLink to='/events' className="group-list-header" activeClassName='active'>
+            Events
+          </NavLink>
+          <NavLink to='/groups' className="group-list-header" activeClassName='active'>
+            Groups
+          </NavLink>
       </div>
 
-
-    </>
+      <div className="group-list-caption">Groups in Meetup</div>
+      {groups.map((group) => (
+        <div key={group.id} className="group-list-item" onClick={() => handleGroupClick(group.id)}>
+          <div className="thumbnail"></div>
+          <div className="group-info">
+            <div className="group-name">{group.name}</div>
+            <div className="group-location">{group.city}</div>
+            <div className="group-description">{group.about}</div>
+            <div className="group-events-private">
+              events · {group.private === 'public' ? 'Public' : 'Private'}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
   )
-
-
-
 }
